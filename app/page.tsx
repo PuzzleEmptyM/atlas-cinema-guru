a0a5702cb9ea999896b50476216c4e532ac13e1c
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-11-05 17:29:45 -0600

    reverted yet again

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,8 @@
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import MoviesList from "./components/MoviesList";
-import { useSession } from "next-auth/react";
 
 export default function Page() {
-  const { data: session } = useSession();
-
-  if (!session) return null; // Avoid rendering if not logged in
 
   return (
     <div className="flex h-screen" style={{ backgroundColor: '#00003c' }}>
@@ -16,7 +11,7 @@ export default function Page() {
         <Header />
         <div className="flex flex-grow">
           <Sidebar />
-          <MoviesList userEmail={session.user.email} /> {/* Pass the user email to MoviesList */}
+          <div >hello</div>
         </div>
       </div>
     </div>
