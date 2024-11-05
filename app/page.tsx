29936457a5bb13d283a3bbe22399bcb776fef930
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-11-04 18:26:04 -0600

    reverted change

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 
-const HomePage = () => {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/login"); // Redirect to login if not authenticated
-    }
-  }, [status, router]);
-
-  if (status === "loading") return <p>Loading...</p>;
+export default function Page() {
 
   return (
     <div className="flex h-screen" style={{ backgroundColor: '#00003c' }}>
@@ -30,5 +17,3 @@ const HomePage = () => {
     </div>
   );
 };
-
-export default HomePage;
