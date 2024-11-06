50583d1b660cb8f1fc2edeb9a6ba905200eabcbd
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-11-05 18:22:45 -0600

    new attempt at populating

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,20 @@
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Movies from "./components/Movies";
 
 export default function Page() {
-
   return (
     <div className="flex h-screen" style={{ backgroundColor: '#00003c' }}>
       <div className="flex-1 flex flex-col">
         <Header />
         <div className="flex flex-grow">
           <Sidebar />
-          <div >hello</div>
+          <div className="flex-1 p-4">
+            <Movies />
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
