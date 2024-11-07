7aa6380d754ff62125d4c9d3e310e8f7b354a488
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-11-07 10:19:58 -0600

    updated total pages

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
 
   // State for Pagination (for Home, Favorites, Watch Later)
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(10);
   const [favoritesPage, setFavoritesPage] = useState(1);
   const [watchLaterPage, setWatchLaterPage] = useState(1);
 
