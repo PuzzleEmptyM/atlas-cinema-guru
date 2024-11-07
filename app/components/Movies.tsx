4d1e7798ec05510267a033e769379d1fddd5319d
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-11-07 10:45:24 -0600

    styling

--- app/components/Movies.tsx
+++ app/components/Movies.tsx
@@ -25,7 +25,7 @@ const Movies: React.FC<MoviesProps> = ({ movies }) => {
   const { favorites, watchLater, toggleFavorite, toggleWatchLater } = useMovies();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 justify-items-between">
       {movies.map((movie) => {
         const isFavorited = favorites.some((fav) => fav.id === movie.id);
         const isWatchLater = watchLater.some((wl) => wl.id === movie.id);
